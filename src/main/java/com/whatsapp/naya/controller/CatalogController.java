bdad8f86764d51cd914b508447ed8336b26bb671
Maricica Caraion <maricica.caraion@landisgyr.com>
2024-07-26 19:50:06 +0300

    Added logger

--- src/main/java/com/whatsapp/naya/controller/CatalogController.java
+++ src/main/java/com/whatsapp/naya/controller/CatalogController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class CatalogController
 {
+   private final Logger logger = Logger.getLogger(CatalogController.class.getName());
+
    @GetMapping("/webhook")
    public ResponseEntity<String> getCatalog(@RequestBody String payload) {
       // Process the webhook payload
       System.out.println(payload);
+      logger.info(payload);
       return ResponseEntity.ok("Received");
    }
 }
